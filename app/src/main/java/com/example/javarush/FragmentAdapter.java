f8e5ab6e1f6047c89d433291770cdd2fe8fdd5c8
Abdurahmanov <erjan.abdurahmanov.kg@gmail.com>
2021-06-02 18:55:09 +0600

    update

---
+++ app/src/main/java/com/example/javarush/FragmentAdapter.java
@@ -0,0 +1,37 @@
+package com.example.javarush;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+public class FragmentAdapter extends FragmentStateAdapter {
+    public FragmentAdapter(FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    public FragmentAdapter(Fragment fragment) {
+        super(fragment);
+    }
+
+    public FragmentAdapter(FragmentManager fragmentManager, Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0: {
+                return new FirstFragment();
+            }
+            case 1: {
+                return new SeccondFragment();
+            }
+        }
+        return new FirstFragment();
+    }
+    @Override
+    public int getItemCount () {
+        return 2;
+    }
+}
